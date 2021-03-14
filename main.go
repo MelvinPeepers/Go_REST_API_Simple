b3d11e0a5ceb11ce01a7e4995fa04d1c916f32c4
Melvin <peepersm@gmail.com>
2021-03-13 23:23:11 -0500

    Initial Commit

---
+++ main.go
@@ -0,0 +1,43 @@
+// main.go
+
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+)
+
+func main() {
+	port:= "8080"
+
+	if fromEnv := os.
+								Getenv(
+								"PORT"); fromEnv != 
+								"" {
+		port = fromEnv
+	}
+
+	log.Printf(
+		"Starting up on http://localhost: %s",
+		port)
+
+	r := chi.NewRouter()
+
+	r.Get("/", func(
+		w http.ResponseWriter,
+		r *http.Request) {
+		w.Header().Set(
+			"Content-Type",
+			"text/plain")
+		})
+
+		r.Mount(
+			"/posts",
+			postsResource{}.Routes())
+			
+		log.Fatal(http.ListenAndServe(
+			":" + port, r))
+}
